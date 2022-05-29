5fd555cd8eb576f73bef3723576974f2821eee87
GAEUN SHIN <newsilver1028@naver.com>
2022-05-29 22:14:43 +0900

    style: stepGraph

--- src/routes/detailMember/stepDataGraph/GRAPH_STYLE.ts
+++ src/routes/detailMember/stepDataGraph/GRAPH_STYLE.ts
@@ -1,6 +1,34 @@
+import dayjs from 'dayjs';
+
 const GRAPH_STYLE = {
-  x: 'date',
-  y: 'steps',
+  weeklyGraphBar: {
+    x: 'date',
+    y: 'steps',
+    barWidth: 10,
+  },
+  dayGraphBar: {
+    x: 'time',
+    y: 'steps',
+  },
+  dayGraphAxis: {
+    tickFormat: (x: string) => dayjs(x, 'HH:mm:ss').get('hour'),
+    fixLabelOverlap: true,
+  },
+  axis: {
+    style: {
+      tickLabels: {
+        fontFamily: 'inherit',
+        fontWeight: 100,
+        letterSpacing: '1px',
+        stroke: 'gray',
+        fontSize: 8,
+        marginBlock: '20px',
+      },
+    },
+  },
+  bar: {
+    style: { data: { fill: '#3169c4' } },
+  },
 };
 
 export default GRAPH_STYLE;
