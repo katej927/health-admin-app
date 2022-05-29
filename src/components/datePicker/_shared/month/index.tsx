afdd611da0004163031ce9566fc8746f1b5d32b4
Seonmi Jung <devkatej927@gmail.com>
2022-05-29 17:58:40 +0900

    refactor: date들 정확한 날짜로 설정 중. 클린코드로 리팩토링 중

--- src/components/datePicker/_shared/month/index.tsx
+++ src/components/datePicker/_shared/month/index.tsx
@@ -3,11 +3,17 @@ import { useRecoilState } from 'recoil';
 import { inquiryPeriodState } from 'states';
 
 import week from 'dayjs/plugin/weekOfYear';
+import isBetween from 'dayjs/plugin/isBetween';
+import localeData from 'dayjs/plugin/localeData';
+import 'dayjs/locale/ko';
 
 import styles from './month.module.scss';
 import cn from 'classnames';
 
 dayjs.extend(week);
+dayjs.extend(localeData);
+dayjs.extend(isBetween);
+dayjs.locale('ko');
 
 interface Props {
   assignedDay: Dayjs;
@@ -17,42 +23,101 @@ interface Props {
 
 const Month = ({ assignedDay, onMonthBtnClick, isCurrentMonth }: Props) => {
   const [inquiryPeriod, setInquiryPeriod] = useRecoilState(inquiryPeriodState);
+  const { startDate, endDate } = inquiryPeriod;
 
   const firstWeek = assignedDay.startOf('month').week();
   const lastWeek = assignedDay.endOf('month').week() === 1 ? 53 : assignedDay.endOf('month').week();
 
   const onDayClick = (date: Dayjs) => {
-    console.log('date', date.format('YYYY-MM-DD'));
-    // setInquiryPeriod()
+    if (!startDate && !endDate)
+      setInquiryPeriod((prev) => ({
+        ...prev,
+        startDate: date.format('YYYY-MM-DD'),
+      }));
+    else if (startDate && !endDate) {
+      if (dayjs(startDate).isAfter(dayjs(date)))
+        setInquiryPeriod((prev) => ({
+          ...prev,
+          startDate: date.format('YYYY-MM-DD'),
+        }));
+      else
+        setInquiryPeriod((prev) => ({
+          ...prev,
+          endDate: date.format('YYYY-MM-DD'),
+        }));
+    } else if (startDate && endDate) setInquiryPeriod({ startDate: date.format('YYYY-MM-DD'), endDate: '' });
   };
 
-  console.log('inquiryPeriod', inquiryPeriod.endDate, inquiryPeriod.startDate);
+  // const calendarArr = () => {
+  //   let result: any[] = []; // any 수정하기
+  //   let weeks = firstWeek;
+  //   for (weeks; weeks <= lastWeek; weeks += 1) {
+  //     result = result.concat(
+  //       <tr key={weeks}>
+  //         {Array(7)
+  //           .fill(0)
+  //           .map((data, index) => {
+  //             const days = assignedDay.startOf('year').week(weeks).startOf('week').add(index, 'day');
+  //             const isOtherMonth = assignedDay.format('MM') !== days.format('MM');
+  //             const isSelectedDate = startDate === days.format('YYYY-MM-DD') || endDate === days.format('YYYY-MM-DD');
+  //             const isBetweenDate = dayjs(days).isBetween(startDate, endDate, 'day', '()');
 
-  const calendarArr = () => {
-    let result: any[] = []; // any 수정하기
-    let weeks = firstWeek;
-    for (weeks; weeks <= lastWeek; weeks += 1) {
-      result = result.concat(
-        <tr key={weeks}>
-          {Array(7)
-            .fill(0)
-            .map((data, index) => {
-              const days = assignedDay.startOf('year').week(weeks).startOf('week').add(index, 'day');
-              const isOtherMonth = assignedDay.format('MM') !== days.format('MM');
+  //             return (
+  //               <td
+  //                 key={`day-${index + 1}`}
+  //                 className={cn(styles.day, {
+  //                   [styles.otherMonth]: isOtherMonth,
+  //                   [styles.selectedDate]: isSelectedDate,
+  //                   [styles.betweenDate]: isBetweenDate,
+  //                 })}
+  //               >
+  //                 <button type='button' onClick={() => onDayClick(days)}>
+  //                   {days.format('D')}
+  //                 </button>
+  //               </td>
+  //             );
+  //           })}
+  //       </tr>
+  //     );
+  //   }
+  //   return result;
+  // };
 
-              return (
-                <td key={`day-${index + 1}`} className={cn(styles.day, { [styles.otherMonth]: isOtherMonth })}>
-                  <button type='button' onClick={() => onDayClick(days)}>
-                    {days.format('D')}
-                  </button>
-                </td>
-              );
-            })}
-        </tr>
-      );
+  const dates = Array.from({ length: assignedDay.daysInMonth() + assignedDay.startOf('month').day() }, (v, index) =>
+    assignedDay.startOf('year').week(firstWeek).startOf('week').add(index, 'day')
+  );
+
+  // type TDate = number | string;
+  // const init: TDate[][] = [];
+
+  // let rowIdx = -1;
+  // const convertedDate = shownDates.reduce((acc, cur, i) => {
+  //   if (!(i % 7)) {
+  //     acc.push([cur]);
+  //     rowIdx += 1;
+  //   } else {
+  //     acc[rowIdx].push(cur);
+  //   }
+  //   return acc;
+  // }, init);
+
+  const init: Dayjs[][] = [];
+
+  let rowIdx = -1;
+  const convertedDate = dates.reduce((acc, cur, i) => {
+    if (!(i % 7)) {
+      acc.push([cur]);
+      rowIdx += 1;
+    } else {
+      acc[rowIdx].push(cur);
     }
-    return result;
-  };
+    return acc;
+  }, init);
+
+  // const days = assignedDay.startOf('year').week(weeks).startOf('week').add(index, 'day');
+  // const isOtherMonth = assignedDay.format('MM') !== days.format('MM');
+  // const isSelectedDate = startDate === days.format('YYYY-MM-DD') || endDate === days.format('YYYY-MM-DD');
+  // const isBetweenDate = dayjs(days).isBetween(startDate, endDate, 'day', '()');
 
   return (
     <div>
@@ -62,7 +127,7 @@ const Month = ({ assignedDay, onMonthBtnClick, isCurrentMonth }: Props) => {
             이전 달
           </button>
         )}
-        <span>{assignedDay.format('YYYY년 MM월')}</span>
+        <time>{assignedDay.format('YYYY년 MM월')}</time>
         {!isCurrentMonth && (
           <button type='button' onClick={() => onMonthBtnClick(true)}>
             다음 달
@@ -70,7 +135,22 @@ const Month = ({ assignedDay, onMonthBtnClick, isCurrentMonth }: Props) => {
         )}
       </div>
       <table>
-        <tbody>{calendarArr()}</tbody>
+        <thead>
+          <tr>
+            {dayjs.weekdaysMin().map((weekday) => (
+              <th key={weekday}>{weekday}</th>
+            ))}
+          </tr>
+        </thead>
+        <tbody>
+          {convertedDate.map((eachWeek: Dayjs[], idx: number) => (
+            <tr key={`week-${idx + 1}st`}>
+              {eachWeek.map((date, index) => (
+                <td key={`date-${index + 1}`}>{date.format('D')}</td>
+              ))}
+            </tr>
+          ))}
+        </tbody>
       </table>
     </div>
   );
--- src/components/datePicker/index.tsx
+++ src/components/datePicker/index.tsx
@@ -8,8 +8,13 @@ import styles from './datePicker.module.scss';
 
 const DatePicker = () => {
   const fixedToday = '2022-04-20';
+  const registrationDate = '2022-02-01'; // 회원 가입일 받아오기
+
   const [getTime, setTime] = useState(dayjs(fixedToday));
   const [inquiryPeriod, setInquiryPeriod] = useRecoilState(inquiryPeriodState);
+  const { startDate, endDate } = inquiryPeriod;
+
+  const INPUT_DATE_VALUE = [startDate, endDate];
 
   const nextMonth = getTime.add(1, 'month');
 
@@ -17,16 +22,30 @@ const DatePicker = () => {
     isAddMonth ? setTime(getTime.add(1, 'month')) : setTime(getTime.subtract(1, 'month'));
   };
 
-  console.log('test', getTime.add(1, 'month'));
-
   return (
-    <div>
+    <fieldset className={styles.wrapper}>
+      <legend className={styles.title}>가입일 조회</legend>
+      {INPUT_DATE_VALUE.map((date, idx) => {
+        return (
+          <input
+            key={`date-input-${idx + 1}`}
+            className={styles.dateInput}
+            type='date'
+            readOnly
+            value={date}
+            required
+            pattern='\d{4}-\d{2}-\d{2}'
+          />
+        );
+      })}
+      <div className={styles.monthsWrapper}>
+        <Month assignedDay={getTime} onMonthBtnClick={onMonthBtnClick} isCurrentMonth />
+        <Month assignedDay={nextMonth} onMonthBtnClick={onMonthBtnClick} isCurrentMonth={false} />
+      </div>
       {/* {BTN_OPTIONS.map((option) => (
         <Button key={option} text={option} />
       ))} */}
-      <Month assignedDay={getTime} onMonthBtnClick={onMonthBtnClick} isCurrentMonth />
-      <Month assignedDay={nextMonth} onMonthBtnClick={onMonthBtnClick} isCurrentMonth={false} />
-    </div>
+    </fieldset>
   );
 };
 
