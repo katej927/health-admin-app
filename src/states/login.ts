7e5af5fb47314b0400391b73dd74ef7dfb6b8d13
daseuls <dls2tmfs2@naver.com>
2022-05-30 15:56:45 +0900

    feat: login ts 수정한부분 다시 수정

--- src/states/login.ts
+++ src/states/login.ts
@@ -2,11 +2,7 @@ import store from 'store';
 import { atom } from 'recoil';
 
 import { ILoginState } from 'types/admin';
-import { LOGIN_DB_KEY, ADMIN_LIST_DB_KEY } from '../constant/key';
-
-import adminList from '../data/admin_data.json';
-
-store.set(ADMIN_LIST_DB_KEY, adminList);
+import { LOGIN_DB_KEY } from '../constant/key';
 
 const admin: ILoginState = store.get(LOGIN_DB_KEY);
 
