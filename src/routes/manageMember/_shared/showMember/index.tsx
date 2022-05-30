242a50978f2a8710c2ed704356a7f548c9ab6fb4
rlaebqebq <rlaebqebq@gmail.com>
2022-05-30 17:57:23 +0900

    feat : 데이터 받아오는 부분 recoil 추가

--- src/routes/manageMember/_shared/showMember/index.tsx
+++ src/routes/manageMember/_shared/showMember/index.tsx
@@ -1,64 +1,49 @@
 import { Link } from 'react-router-dom';
-import { useRecoilState } from 'recoil';
-import { selectMemberState } from 'states/selectMember';
+import { useRecoilState, useRecoilValue } from 'recoil';
+import { searchMemberList } from 'states/searchMemberList';
+import { selectMemberState, IMember } from 'states/selectMember';
+import styles from './showMember.module.scss';
 
 const ShowMember = () => {
   const header = ['회원번호', '가입일', '로그인ID', '상세'];
-  // const members = getMemberData(검색조건);
-  const members = [
-    {
-      id: 1,
-      username: 'dohee',
-      crt_ymdt: '2022-02-26 11:00:29',
-    },
-    {
-      id: 2,
-      username: 'daseul',
-      crt_ymdt: '2022-04-16 06:11:40',
-    },
-    {
-      id: 3,
-      username: 'minhyo',
-      crt_ymdt: '2022-04-17 00:21:10',
-    },
-  ];
-
+  const originMembers = useRecoilValue(searchMemberList);
   const [, setSelectMember] = useRecoilState(selectMemberState);
+  const members = originMembers as IMember[];
 
   return (
-    <div>
-      <p>
-        전체 총 <span>{members.length}명의 회원이 검색되었습니다.</span>
-      </p>
-      <table>
-        <thead>
-          <tr>
-            {header.map((headerName, index) => {
-              const key = `${headerName}-${index}`;
-              return <th key={key}>{headerName}</th>;
+    <div className={styles.boxWrapper}>
+      <p>전체 총 {members.length}명의 회원이 검색되었습니다.</p>
+      <div className={styles.tableWrapper}>
+        <table>
+          <thead>
+            <tr>
+              {header.map((headerName, index) => {
+                const key = `${headerName}-${index}`;
+                return <th key={key}>{headerName}</th>;
+              })}
+            </tr>
+          </thead>
+          <tbody>
+            {members.map((member, index) => {
+              const key = `${member}-${index}`;
+              return (
+                <tr key={key}>
+                  <td>{member.id}</td>
+                  <td>{member.crt_ymdt}</td>
+                  <td>{member.username}</td>
+                  <td>
+                    <Link to='/detailMember'>
+                      <button type='button' onClick={() => setSelectMember(member)}>
+                        상세보기
+                      </button>
+                    </Link>
+                  </td>
+                </tr>
+              );
             })}
-          </tr>
-        </thead>
-        <tbody>
-          {members.map((member, index) => {
-            const key = `${member}-${index}`;
-            return (
-              <tr key={key}>
-                <td>{member.id}</td>
-                <td>{member.crt_ymdt}</td>
-                <td>{member.username}</td>
-                <td>
-                  <Link to='/detailMember'>
-                    <button type='button' onClick={() => setSelectMember(member)}>
-                      상세보기
-                    </button>
-                  </Link>
-                </td>
-              </tr>
-            );
-          })}
-        </tbody>
-      </table>
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 };
